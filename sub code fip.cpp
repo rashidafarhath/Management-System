6094961ae9524c15bbaaa3b5ee492567fb5b803e
Rashida Farhath <rashidafarhath0007@gmail.com>
2023-05-23 21:00:47 +0530

    Update sub code fip.cpp

--- sub code fip.cpp
+++ sub code fip.cpp
@@ -5,7 +5,6 @@
 #include<iostream>
 
 using namespace std;
-
 class program
 {
           char program_number[30];
@@ -17,20 +16,20 @@ class program
   public:
           void get_program_details()
           {
-			cout<<"\nENTER DETAILS ABOUT THE PROGRAM\n";
+		    cout<<"\nENTER DETAILS ABOUT THE PROGRAM\n";
                     cout<<"\nEnter The Program Number: ";
                     cin>>program_number;
                     cout<<"\nEnter The Name of The Program: ";
-                     cin.ignore();
+                    cin.ignore();
                     cin.getline(program_name,50);
                     cout<<"\nEnter The Assessor's Name: ";
                     cin.ignore();
                     cin.getline(accessor_name,50);
                     fflush(stdin);
                     cout<<"\nEnter The Fee : ";
-				cin>>fee;
-			cout<<"\nEnter The Duration (years) : ";
-				cin>>duration;
+		    cin>>fee;
+		    cout<<"\nEnter The Duration (years) : ";
+		    cin>>duration;
           }
 
           void display_program()
@@ -41,6 +40,7 @@ class program
                     cout<<"\nDuration (years) : "<<duration;
                     cout<<"\nFee : "<<fee;
           }
+	
           void update_program()
           {
                     cout<<"\nProgram number : "<<program_number;
@@ -51,62 +51,64 @@ class program
                     cin.ignore();
                     cin.getline(accessor_name,50);
                     fflush(stdin);
-				cout<<"\nEnter Duration (years) : ";
-				cin>>duration;
-                                                          
+		    cout<<"\nEnter Duration (years) : ";
+		    cin>>duration;                                                          
           }
+	
           char* getprogramnumber()
           {
                     return program_number;
           }
+	
           void report()
-          {cout<<program_number<<setw(30)<<program_name<<setw(30)<<accessor_name<<setw(30)<<duration<<setw(30)<<fee<<endl;}
+          {
+		    cout<<program_number<<setw(30)<<program_name<<setw(30)<<accessor_name<<setw(30)<<duration<<setw(30)<<fee<<endl;
+	  }
 
-};        
-
-
+          };        
 
 fstream fp;
 program pro;
-void add_program()
-{
+         void add_program()
+         {
           system("cls");
           int more_or_main;
           fp.open("program.dat",ios::out|ios::app);
           do
           {
-                    pro.get_program_details();
-                    fp.write((char*)&pro,sizeof(program));
-                    cout<<"\nPress 1 to add more programs.";
-                    cout<<"\nPress 2 to return to main menu.\n";
-                    cout<<"Enter: ";
-                    cin>>more_or_main;
-          }while(more_or_main == 1);
+                   pro.get_program_details();
+                   fp.write((char*)&pro,sizeof(program));
+                   cout<<"\nPress 1 to add more programs.";
+                   cout<<"\nPress 2 to return to main menu.\n";
+                   cout<<"Enter: ";
+                   cin>>more_or_main;
+          }
+          while(more_or_main == 1);
           fp.close();
-}
+          }
 
-void display_a_program(char n[])
-{
+          void display_a_program(char n[])
+          {
           system("cls");
           cout<<"\nPROGRAM DETAILS\n";
           int check=0;
           fp.open("program.dat",ios::in);
           while(fp.read((char*)&pro,sizeof(program)))
           {
-                    if(strcmpi(pro.getprogramnumber(),n)==0)
-                    {
-                               pro.display_program();
-                              check=1;
+                   if(strcmpi(pro.getprogramnumber(),n)==0)
+                   {
+                          pro.display_program();
+                          check=1;
                     }
           }
           fp.close();
           if(check==0)
-                    cout<<"\n\nProgram does not exist";
-        getch();
-}
+                     cout<<"\n\nProgram does not exist";
+          getch();
+          }
 
-void update_program()
-{
+          void update_program()
+          {
           system("cls");
           char n[20];
           int found=0;
@@ -132,10 +134,10 @@ void update_program()
           if(found==0)
                     cout<<"\n\n Record Not Found ";
           getch();
-}
+          }
 
-void delete_program()
-{
+          void delete_program()
+          {
           system("cls");
           char n[20];
           int flag=0;
@@ -164,10 +166,10 @@ void delete_program()
           else
                     cout<<"\n\nRecord not found";
           getch();
-}
+          }
 
- void View_institute()
-{
+          void View_institute()
+          {
           system("cls");
           char n[20];
           int flag=0;
@@ -176,9 +178,10 @@ void delete_program()
           cout<<"\n\nNO 83 Katugastota road Kandy";
           cout<<"\n\0758760924";
           getch();
-}
-void display_all_program()
-{
+          }
+
+          void display_all_program()
+          {
           system("cls");
           fp.open("program.dat",ios::in);
           if(!fp)
@@ -188,7 +191,7 @@ void display_all_program()
                     return;
           }
           cout<<"\n\n\t\tPROGRAM LIST\n\n";
-         cout<<"=======================================================================================================================\n";
+          cout<<"=======================================================================================================================\n";
           cout<<"Program Number"<<setw(20)<<"Program Name"<<setw(27)<<"Assessor"<<setw(27)<<"Duration"<<setw(27)<<"Fee"<<endl;
           cout<<"=======================================================================================================================\n";
           while(fp.read((char*)&pro,sizeof(program)))
@@ -197,15 +200,16 @@ void display_all_program()
           }
           fp.close();
           getch();
-}
+          }
 
-void intro()
-{
-     system("color 03");
-     system("cls");
-}
-class student {
-	     char student_number[30];
+          void intro()
+          {
+                    system("color 03");
+                    system("cls");
+          }
+
+          class student {
+	  char student_number[30];
           char student_name[50];
           char program_name[20];
           
@@ -213,11 +217,11 @@ class student {
   public:
           void get_student_details()
           {
-			cout<<"\nENTER DETAILS ABOUT STUDENTS\n";
+		    cout<<"\nENTER DETAILS ABOUT STUDENTS\n";
                     cout<<"\nEnter The Student Number: ";
                     cin>>student_number;
                     cout<<"\nEnter The Name of The Student: ";
-                     cin.ignore();
+                    cin.ignore();
                     cin.getline(student_name,50);
                     cout<<"\nEnter The Program : ";
                     cin.ignore();
@@ -272,7 +276,7 @@ void display_all_student()
                     return;
           }
           cout<<"\n\n\t\tSTUDENT LIST\n\n";
-         cout<<"===========================================================================\n";
+          cout<<"===========================================================================\n";
           cout<<"Student Number"<<setw(20)<<"Student Name"<<setw(27)<<"Program"<<endl;
           cout<<"===========================================================================\n";
           while(ffp.read((char*)&sstd,sizeof(student)))
@@ -325,45 +329,43 @@ int main()
           int option = 0;
           for(;;)
           {
-               intro();
+            intro();
             cout<<"\t\tMaster Mind Institue";
             cout<<"\n\t\tMAIN MENU";
             cout<<"\n\t\t-------------------------------------------------\n";
             cout<<"\n\t\tPlease select the option";
-                cout<<"\n\t\tPress 1 to ADD PROGRAM";
-                cout<<"\n\t\tPress 2 to DISPLAY ALL PROGRAMS";
-                cout<<"\n\t\tPress 3 to SEARCH PROGRAM";
-                cout<<"\n\t\tPress 4 to UPDATE PROGRAM";
-                cout<<"\n\t\tPress 5 to DELETE PROGRAM";
-                cout<<"\n\t\tPress 6 to REGISTER STUDENT";
-                cout<<"\n\t\tPress 7 to VIEW STUDENT";
-                cout<<"\n\t\tPress 8 to INSTITUTE DETAILS";
-                cout<<"\n\t\tPress 9 to EXIT";
-			cout<<"\n\t\t-------------------------------------------------\n";
-			cout<<"\n\t\tPress ENTER in every unit to go back to main menu";
-			cout<<"\n\t\tOption: ";
-			cin>>option;
-			switch(option)
-	{
+            cout<<"\n\t\tPress 1 to ADD PROGRAM";
+            cout<<"\n\t\tPress 2 to DISPLAY ALL PROGRAMS";
+            cout<<"\n\t\tPress 3 to SEARCH PROGRAM";
+            cout<<"\n\t\tPress 4 to UPDATE PROGRAM";
+            cout<<"\n\t\tPress 5 to DELETE PROGRAM";
+            cout<<"\n\t\tPress 6 to REGISTER STUDENT";
+            cout<<"\n\t\tPress 7 to VIEW STUDENT";
+            cout<<"\n\t\tPress 8 to INSTITUTE DETAILS";
+            cout<<"\n\t\tPress 9 to EXIT";
+	    cout<<"\n\t\t-------------------------------------------------\n";
+	    cout<<"\n\t\tPress ENTER in every unit to go back to main menu";
+	    cout<<"\n\t\tOption: ";
+		  
+	    cin>>option;
+	    switch(option)
+	    {
                     case 1:  system("cls");
-                                         add_program();
-                                                   break;
-                               case 2: display_all_program();
-                                 break;
+                          add_program();break;
+                    case 2: display_all_program();break;
                     case 3:
-                              char num[20];
-                               system("cls");
-                              cout<<"\n\n\tPlease Enter The Program No. ";
-                              cin>>num;
-                              display_a_program(num);
-                              break;
+                          char num[20];
+                          system("cls");
+                          cout<<"\n\n\tPlease Enter The Program No. ";
+                          cin>>num;
+                          display_a_program(num);
+                          break;
                     case 4: update_program();break;
                     case 5: delete_program();break;
                     case 6: add_student();break;
-					case 7: display_all_student();break;
-					case 8: View_institute();break;
-                    case 9: exit(0);
-                               break;
+		    case 7: display_all_student();break;
+		    case 8: View_institute();break;
+                    case 9: exit(0);break;
                     default:cout<<"\a";
           }     
 
